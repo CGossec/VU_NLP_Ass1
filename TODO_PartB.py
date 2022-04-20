3ddee2dfffadcd0ff19689f7c28b8cb45e48556c
Mickey <mieke95_8@hotmail.com>
2022-04-20 15:28:59 +0200

    Q7 done

--- TODO_PartB.py
+++ TODO_PartB.py
@@ -10,8 +10,32 @@ nlp = spacy.load("en_core_web_sm")
 
 #Extracting the columns Target word (5), binary label(10) and probabilistic label(11)
 df = df_old[[4,9,10]]
-df.to_csv('data/original/english/WikiNews_Train_extracted.tsv', sep='\t', header=None, index=False)
-doc = nlp(open(os.path.join(os.getcwd(), "data/original/english/WikiNews_Train_extracted.tsv"),encoding="utf8").read())
+df.columns = ["Target word", "Binary", "Probabilistic"]
+
+print(f"Total number of instances labeled 0 and 1 in doc are: {df['Binary'].value_counts()}")
+print(f"Min, max, median, mean, and stdev of the probabilistic label in doc are: {df['Probabilistic'].min()}, "
+      f"{df['Probabilistic'].max()}, {df['Probabilistic'].median()}, {df['Probabilistic'].mean()} "
+      f"and {df['Probabilistic'].std()}.")
+
+#df.to_csv('data/original/english/WikiNews_Train_extracted.tsv', sep='\t', header=None, index=False)
+
+counter = 0
+max = 0
+for row in range(len(df)):
+    doc = nlp(df.loc[row, "Target word"])
+    counter_row = 0
+    for token in doc:
+        counter_row = counter_row + 1
+        if counter_row > max:
+            max = counter_row
+    if counter_row > 1:
+        counter = counter + 1
+
+print(f"Number of instances consisting of more than one token are: {counter}")
+print(f"Maximum number of tokens for an instance is: {max}")
+
+
+
 
 
 
