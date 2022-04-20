9c0cec1642583c72e33ae638e11d8b3788624d7a
Celian GOSSEC <celian.gossec@gmail.com>
2022-04-20 10:46:34 +0200

    Compute F measure

--- TODO_detailed_evaluation.py
+++ TODO_detailed_evaluation.py
@@ -1,2 +1,15 @@
 # The original code only outputs the accuracy and the loss.
 # Process the file model_output.tsv and calculate precision, recall, and F1 for each class
+import os
+
+def compute_recall() -> float:
+    pass
+
+def compute_precision() -> float:
+    pass
+
+def compute_F_measure(recall: float, precision: float, beta: float = 1) -> float:
+    return (beta ** 2 + 1) * precision * recall / (beta ** 2 * (precision + recall))
+
+if __name__ == '__main__':
+    pred_file = os.path.join(os.getcwd(), "experiments/base_model/model_output.tsv")
