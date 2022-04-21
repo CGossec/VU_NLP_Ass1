9ee65dadb9d8b866904cfad102ffb244e0747aba
Ahmet Zafer Saglik <zafersaglk@gmail.com>
2022-04-21 16:34:56 +0200

    Part B 10 has been added

--- TODO_baselines.py
+++ TODO_baselines.py
@@ -5,33 +5,107 @@
 # Frequency baseline: determines the class based on a frequency threshold
 
 from model.data_loader import DataLoader
+import random
+random.seed(10)
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score
 
 # Each baseline returns predictions for the test data. The length and frequency baselines determine a threshold using the development data.
+def split(word):
+        return [char for char in word] 
+def preprocessing(testinput,testlabels):
+    'Preprocess and return a pandas df'
+    label_arr=[]
+    token_arr=[]
+    for instance,labels in zip(testinput,testlabels):
+        tokens = instance.split(" ")
+        labels=split(labels.replace(' ', ''))
+        for token,label in zip(tokens,labels):
+            token_arr.append(token)
+            label_arr.append(label)
+        token_arr.append("--")
+        label_arr.append("--")
+    df = pd.DataFrame(list(zip(token_arr, label_arr)),
+               columns =['Tokens', 'Labels'])
+    return df
+
+
 
 def majority_baseline(train_sentences, train_labels, testinput, testlabels):
-    predictions = []
-
     # TODO: determine the majority class based on the training data
     # ...
-    majority_class = "X"
-    predictions = []
-    for instance in testinput:
-        tokens = instance.split(" ")
-        instance_predictions = [majority_class for t in tokens]
-        predictions.append(instance, instance_predictions)
-
+    
+    df=preprocessing(testinput,testlabels)
+    majority_class = df['Labels'].mode()[0]
+    
+    df["prediction"]=majority_class
+    df["prediction"]=np.where(df["Labels"]=="--","--",df["prediction"])
+    
     # TODO: calculate accuracy for the test input
     # ...
-    return accuracy, predictions
+    accuracy=accuracy_score(df["Labels"], df["prediction"])
+    return accuracy, df
+    
 
 
+def random_baseline(train_sentences, train_labels, testinput, testlabels):
+    # TODO: determine the majority class based on the training data
+    # ...
+    df=preprocessing(testinput,testlabels)
+    
+    rand_list=df["Labels"].unique()
+    rand_list=rand_list.tolist()
+    rand_list.remove('--')
 
+    def random_class(row):
+        rand_value=random.choice(rand_list)
+        return rand_value
+    
+    
+    df["prediction"]=df["Labels"].apply(random_class)
+    df["prediction"]=np.where(df["Labels"]=="--","--",df["prediction"])
+    
+    # TODO: calculate accuracy for the test input
+    # ...
+    accuracy=accuracy_score(df["Labels"], df["prediction"])
+    return accuracy, df
+
+
+def length_baseline(train_sentences, train_labels, testinput, testlabels):
+    # TODO: determine the majority class based on the training data
+    # ...
+    length=7
+    df=preprocessing(testinput,testlabels)
+    
+    df["prediction"]=np.where(df["Tokens"].str.len()>length,"C","N")
+    df["prediction"]=np.where(df["Labels"]=="--","--",df["prediction"])
+
+    accuracy=accuracy_score(df["Labels"], df["prediction"])
+    # TODO: calculate accuracy for the test input
+    # ...
+    return accuracy, df
+
+def frequency_baseline(train_sentences, train_labels, testinput, testlabels):
+    # TODO: determine the majority class based on the training data
+    # ...
+    freq=2
+    df=preprocessing(testinput,testlabels)
+    df["freq"]=df['Tokens'].map(df['Tokens'].value_counts())
+    df["prediction"]=np.where(df["freq"]>=freq,"N","C")
+    df["prediction"]=np.where(df["Labels"]=="--","--",df["prediction"])
+    accuracy=accuracy_score(df["Labels"], df["prediction"])
+    # TODO: calculate accuracy for the test input
+    # ...
+    #Before returning df I am deleting freq column for aligning other baselines
+    df.drop('freq', inplace=True, axis=1)
+    return accuracy, df
 
 
 if __name__ == '__main__':
-    train_path = "data/preprocessed/train"
-    dev_path = "data/preprocessed/dev"
-    test_path = "data/preprocessed/test"
+    train_path = "data/preprocessed/train/"
+    dev_path = "data/preprocessed/val/"
+    test_path = "data/preprocessed/test/"
 
     # Note: this loads all instances into memory. If you work with bigger files in the future, use an iterator instead.
 
@@ -50,8 +124,30 @@ if __name__ == '__main__':
     with open(test_path + "sentences.txt",encoding='utf-8') as testfile:
         testinput = testfile.readlines()
 
-    with open("test_path + labels.txt",encoding='utf-8') as test_labelfile:
+    with open(test_path + "labels.txt",encoding='utf-8') as test_label_file:
         testlabels = test_label_file.readlines()
-    majority_accuracy, majority_predictions = majority_baseline(train_sentences, train_labels, testinput)
 
+    #Returning result dfs and acc for both test and dev
+    majority_accuracy_test, majority_predictions_test_df = majority_baseline(train_sentences, train_labels, testinput,testlabels)
+    majority_accuracy_dev, majority_predictions_dev_df = majority_baseline(train_sentences, train_labels, dev_sentences,dev_labels)
+
+    random_accuracy_test, random_predictions_test_df = random_baseline(train_sentences, train_labels, testinput,testlabels)
+    random_accuracy_dev, random_predictions_dev_df = random_baseline(train_sentences, train_labels, dev_sentences,dev_labels)
+
+    length_accuracy_test, length_predictions_test_df = length_baseline(train_sentences, train_labels, testinput,testlabels)
+    length_accuracy_dev, length_predictions_dev_df = length_baseline(train_sentences, train_labels, dev_sentences,dev_labels)
+
+    freq_accuracy_test, freq_predictions_test_df = frequency_baseline(train_sentences, train_labels, testinput,testlabels)
+    freq_accuracy_dev, freq_predictions_dev_df = frequency_baseline(train_sentences, train_labels, dev_sentences,dev_labels)
+    
     # TODO: output the predictions in a suitable way so that you can evaluate them
+    print("majority_accuracy_test",majority_accuracy_test)
+    print("majority_accuracy_dev",majority_accuracy_dev)
+    print("random_accuracy_test",random_accuracy_test)
+    print("random_accuracy_dev",random_accuracy_dev)
+    print("length_accuracy_test",length_accuracy_test)
+    print("length_accuracy_dev",length_accuracy_dev)
+    print("freq_accuracy_test",freq_accuracy_test)
+    print("freq_accuracy_dev",freq_accuracy_dev)
+
+    
